68bfb771c264b3dfebb128facd2d91542b92598f
Micuks <sdkjwql@gmail.com>
2023-03-17 11:20:07 +0800

    enhancement: Written interfaces of AST nodes in code generation. Implementes visit_program, visit_binop, visit_num and visit

--- src/code_generation/code_generator.h
+++ src/code_generation/code_generator.h
@@ -1,5 +1,7 @@
 #ifndef PASCAL2C_SRC_CODE_GENERATION_CODE_GENERATION_H_
 #define PASCAL2C_SRC_CODE_GENERATION_CODE_GENERATION_H_
+#include <memory>
+#include <sstream>
 #include <string>
 
 #include "ast_interface.h"
@@ -8,15 +10,39 @@
 namespace pascal2c {
 namespace code_generation {
 using string = ::std::string;
+template <typename T> using vector = ::std::vector<T>;
+
 class CodeGenerator {
   public:
-    CodeGenerator(const parser::Parser *parser) : parser_(parser) {}
+    explicit CodeGenerator(std::shared_ptr<parser::Parser> parser)
+        : parser_(parser) {}
+    // TODO: Output of Visit save into ostream
     void Visit(const semantic::ASTRoot &ast);
+    int Interpret();
     string GetCCode();
 
   private:
-    const parser::Parser *parser_;
+    int Visit(const std::shared_ptr<semantic::ASTNode> &node);
+    int VisitBinOp(const std::shared_ptr<semantic::BinOp> &node);
+    int VisitNum(const std::shared_ptr<semantic::Num> &node);
+    void VisitProgram(const std::shared_ptr<semantic::Program> &node);
+    void VisitBlock(const std::shared_ptr<semantic::Block> &node);
+    int VisitVarDecl(const std::shared_ptr<semantic::VarDecl> &node);
+    int VisitCompound(const std::shared_ptr<semantic::Compound> &node);
+    int VisitAssign(const std::shared_ptr<semantic::Assign> &node);
+    int VisitVar(const std::shared_ptr<semantic::Var> &node);
+    int VisitNoOp(const std::shared_ptr<semantic::NoOp> &node);
+
+    // Parser
+    std::shared_ptr<parser::Parser> parser_;
+    // AST root node
     semantic::ASTRoot ast_;
+    // Global Scope symbols
+    vector<semantic::ASTNode> global_scope_;
+    // ostream
+    std::stringstream ostream_;
+    // Current indent level
+    int indent_level_;
 };
 } // namespace code_generation
 } // namespace pascal2c
