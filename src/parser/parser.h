fac834b029a9181d45043d12ad114001e6ca7a2c
Ascian <2206019421@qq.com>
2023-03-28 22:58:44 +0800

    update parser.h

--- src/parser/parser.h
+++ src/parser/parser.h
@@ -36,68 +36,156 @@ namespace pascal2c::parser
     class Parser
     {
     public:
+        // param:
+        //     in is the input file
         explicit Parser(FILE *in);
 
     private:
-        FRIEND_TEST(TokenTest,TestNextToken);
-        FRIEND_TEST(ExprParserTest,TestParsePrimary);
-        FRIEND_TEST(ExprParserTest,TestParseExpr);
-        FRIEND_TEST(StatementParserTest,TestAssignStatement);
+        FRIEND_TEST(TokenTest, TestNextToken);
+        FRIEND_TEST(ExprParserTest, TestParsePrimary);
+        FRIEND_TEST(ExprParserTest, TestParseExpr);
+        FRIEND_TEST(StatementParserTest, TestAssignStatement);
         FRIEND_TEST(StatementParserTest, TestIfStatement);
         FRIEND_TEST(StatementParserTest, TestForStatement);
         FRIEND_TEST(StatementParserTest, TestCompoundStatement);
 
-        int token_, next_token_;
+        int token_, next_token_; // current token and next token
+
+        // if an lex error occur, error number will be stored in this variable
         int lexer_errno_;
+
+        // token value
         YYSTYPE tok_value_, next_tok_value_;
         int line_, next_line_;     // line number of token in the input file
         int column_, next_column_; // column number of token in the input file
         std::string text_, next_text_;
 
-        std::shared_ptr<ast::Expression> (Parser::*prefix_parser_[302])();
-
         vector<std::string> err_msg_; // error massages
 
+        // get next token
+        // return:
+        //     the next token
         int NextToken();
 
+        // match token
+        // param:
+        //     token is the token to match
+        // throw:
+        //     SyntaxErr if token not match
         void Match(int token);
 
+        // get the error message of lexer
+        // return:
+        //     the error message
         std::string GetLexerErrMsg();
 
+        // parse the whole program
+        // eg. program p; const a = 1; var b : integer; function f(a : integer) : integer; begin end; begin end.
+        // return:
+        //     the ast of the program
         std::shared_ptr<ast::Program> ParseProgram();
 
+        // parse the program head
+        // eg. program p(a, b, c);
+        // return:
+        //     the ast of the program head
         std::shared_ptr<ast::ProgramHead> ParseProgramHead();
 
+        // parse the program body
+        // eg. const a = 1; var b : integer; function f(a : integer) : integer; begin end; begin end.
+        // return:
+        //     the ast of the program body
         std::shared_ptr<ast::ProgramBody> ParseProgramBody();
 
+        // parse the const declaration
+        // eg. const a = 1;
+        // return:
+        //     the ast of the const declaration
         std::shared_ptr<ast::ConstDeclaration> ParseConstDeclaration();
 
+        // parse the var declaration
+        // eg. var a : integer;
+        // return:
+        //     the ast of the var declaration
         std::shared_ptr<ast::VarDeclaration> ParseVarDeclaration();
 
+        // parse the subprogram declaration
+        // eg. function f(a : integer) : integer; begin end;
+        // eg. procedure p(a : integer); begin end;
+        // return:
+        //     the ast of the subprogram declaration
         std::shared_ptr<ast::Subprogram> ParseSubprogram();
 
+        // parse the subprogram head
+        // eg. function f(a : integer) : integer;
+        // eg. procedure p(a : integer);
+        // return:
+        //     the ast of the subprogram head
         std::shared_ptr<ast::SubprogramHead> ParseSubprogramHead();
 
+        // parse the subprogram body
+        // eg. begin end;
+        // return:
+        //     the ast of the subprogram body
         std::shared_ptr<ast::SubprogramBody> ParseSubprogramBody();
 
+        // parse the id list
+        // eg. a, b, c
+        // return:
+        //     the ast of the id list
         std::shared_ptr<ast::IdList> ParseIdList();
 
+        // parse the type
+        // eg. integer
+        // eg. array[1..10] of integer
+        // return:
+        //     the ast of the type
         std::shared_ptr<ast::Type> ParseType();
 
+        // parse the period
+        // eg. 1..10
+        // return:
+        //     the ast of the period
         ast::Type::Period ParsePeriod();
 
+        // parse the parameter
+        // eg. a : integer
+        // eg. var a : integer
+        // return:
+        //     the ast of the parameter
         std::shared_ptr<ast::Parameter> ParseParameter();
 
+        // parse the expression
+        // eg. a + b
+        // return:
+        //     the ast of the expression
         std::shared_ptr<ast::Expression> ParseExpr();
 
+        // parse the expression
+        // param:
+        //     prec is the precedence of the operator
+        // return:
+        //     the ast of the expression
         std::shared_ptr<ast::Expression> ParseExpr(int prec);
 
+        // parse the expression list
+        // return:
+        //     the ast of the expression list
         vector<std::shared_ptr<ast::Expression>> ParseExprList();
 
+        // parse the primary unit of expression
+        // return:
+        //     the ast of the primary unit of expression
         std::shared_ptr<ast::Expression> ParsePrimary();
 
+        // parse the parenthesis
+        // return:
+        //     the ast of the parenthesis
         std::shared_ptr<ast::Expression> ParseParen();
 
+        // parse the prefix
+        // return:
+        //     the ast of the prefix
         std::shared_ptr<ast::Expression> ParsePrefix();
 
         std::shared_ptr<ast::Expression> ParseNumber();
