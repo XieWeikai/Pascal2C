b82b3bffbbedab93b8d73dec9a4c4ab51e6a3ea7
Micuks <sdkjwql@gmail.com>
2023-04-25 09:58:53 +0800

    [generator] feat: Visitor Accept method for Program, Block, Declaration, Vardecl, Compound, BinOp, Num, Type, Assign, Var and NoOp

--- src/code_generation/ast_adapter.cc
+++ src/code_generation/ast_adapter.cc
@@ -4,6 +4,8 @@
 
 namespace pascal2c {
 namespace code_generation {
+using ::std::dynamic_pointer_cast;
+
 ASTNode::~ASTNode() {}
 VarDecl::~VarDecl() {
     var_node_.reset();
@@ -15,18 +17,37 @@ Var::~Var() { token_.reset(); }
 Oper::~Oper() { oper_.reset(); }
 Num::~Num() { token_.reset(); }
 
+// Program
 void Program::Accept(Visitor &visitor) {
-    visitor.VisitProgram(
-        std::dynamic_pointer_cast<Program>(shared_from_this()));
+    visitor.VisitProgram(dynamic_pointer_cast<Program>(shared_from_this()));
 }
 
+// Block
+void Block::Accept(Visitor &visitor) {
+    visitor.VisitBlock(dynamic_pointer_cast<Block>(shared_from_this()));
+}
 Block::Block(const std::shared_ptr<Declaration> &declarations,
              const std::shared_ptr<Compound> &compound_statement)
     : declarations_(declarations), compound_statement_(compound_statement){};
 
+// Declaration
+void Declaration::Accept(Visitor &visitor) {
+    visitor.VisitDeclaration(
+        dynamic_pointer_cast<Declaration>(shared_from_this()));
+}
+
+// VarDecl
+void VarDecl::Accept(Visitor &visitor) {
+    visitor.VisitVarDecl(dynamic_pointer_cast<VarDecl>(shared_from_this()));
+}
+
+// Compound
 Compound::Compound(const std::vector<std::shared_ptr<ASTNode>> &children)
     : children_(
           const_cast<std::vector<std::shared_ptr<ASTNode>> &>(children)){};
+void Compound::Accept(Visitor &visitor) {
+    visitor.VisitCompound(dynamic_pointer_cast<Compound>(shared_from_this()));
+}
 
 /**
  * add new node to children
@@ -34,5 +55,36 @@ Compound::Compound(const std::vector<std::shared_ptr<ASTNode>> &children)
 void Compound::AddChild(std::shared_ptr<ASTNode> node) {
     children_.push_back(node);
 }
+
+// BinOp
+void BinOp::Accept(Visitor &visitor) {
+    visitor.VisitBinOp(dynamic_pointer_cast<BinOp>(shared_from_this()));
+}
+
+// Num
+void Num::Accept(Visitor &visitor) {
+    visitor.VisitNum(dynamic_pointer_cast<Num>(shared_from_this()));
+}
+
+// Type
+void Type::Accept(Visitor &visitor) {
+    visitor.VisitType(dynamic_pointer_cast<Type>(shared_from_this()));
+}
+
+// Assign
+void Assign::Accept(Visitor &visitor) {
+    visitor.VisitAssign(dynamic_pointer_cast<Assign>(shared_from_this()));
+}
+
+// Var
+void Var::Accept(Visitor &visitor) {
+    visitor.VisitVar(dynamic_pointer_cast<Var>(shared_from_this()));
+}
+
+// NoOp
+void NoOp::Accept(Visitor &visitor) {
+    visitor.VisitNoOp(dynamic_pointer_cast<NoOp>(shared_from_this()));
+}
+
 } // namespace code_generation
 } // namespace pascal2c
--- src/code_generation/visitor.h
+++ src/code_generation/visitor.h
@@ -8,26 +8,17 @@ namespace code_generation {
 class Visitor {
   public:
     virtual void Visit(const std::shared_ptr<ASTNode> &node) = 0;
-    virtual void
-    VisitBinOp(const std::shared_ptr<code_generation::BinOp> &node) = 0;
-    virtual void
-    VisitNum(const std::shared_ptr<code_generation::Num> &node) = 0;
-    virtual void
-    VisitType(const std::shared_ptr<code_generation::Type> &node) = 0;
-    virtual void
-    VisitProgram(const std::shared_ptr<code_generation::Program> &node) = 0;
-    virtual void
-    VisitBlock(const std::shared_ptr<code_generation::Block> &node) = 0;
-    virtual void
-    VisitVarDecl(const std::shared_ptr<code_generation::VarDecl> &node) = 0;
-    virtual void
-    VisitCompound(const std::shared_ptr<code_generation::Compound> &node) = 0;
-    virtual void
-    VisitAssign(const std::shared_ptr<code_generation::Assign> &node) = 0;
-    virtual void
-    VisitVar(const std::shared_ptr<code_generation::Var> &node) = 0;
-    virtual void
-    VisitNoOp(const std::shared_ptr<code_generation::NoOp> &node) = 0;
+    virtual void VisitProgram(const std::shared_ptr<Program> &node) = 0;
+    virtual void VisitBlock(const std::shared_ptr<Block> &node) = 0;
+    virtual void VisitDeclaration(const std::shared_ptr<Declaration> &node) = 0;
+    virtual void VisitVarDecl(const std::shared_ptr<VarDecl> &node) = 0;
+    virtual void VisitCompound(const std::shared_ptr<Compound> &node) = 0;
+    virtual void VisitBinOp(const std::shared_ptr<BinOp> &node) = 0;
+    virtual void VisitNum(const std::shared_ptr<Num> &node) = 0;
+    virtual void VisitType(const std::shared_ptr<Type> &node) = 0;
+    virtual void VisitAssign(const std::shared_ptr<Assign> &node) = 0;
+    virtual void VisitVar(const std::shared_ptr<Var> &node) = 0;
+    virtual void VisitNoOp(const std::shared_ptr<NoOp> &node) = 0;
 };
 } // namespace code_generation
 } // namespace pascal2c
