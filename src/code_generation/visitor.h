662afe80641ba9aa697e503283d486a43903c46f
Micuks <sdkjwql@gmail.com>
2023-04-26 11:05:17 +0800

    [generator] feat: Code generation for subprogram, function, array. Add generation support for const vars.

--- src/code_generation/visitor.h
+++ src/code_generation/visitor.h
@@ -5,22 +5,30 @@
 #include <memory>
 namespace pascal2c {
 namespace code_generation {
+using ::std::shared_ptr;
 class Visitor {
   public:
-    virtual void Visit(const std::shared_ptr<ASTNode> &node,
+    virtual void Visit(const shared_ptr<ASTNode> &node,
                        bool indent = false) = 0;
-    virtual void VisitProgram(const std::shared_ptr<Program> &node) = 0;
-    virtual void VisitBlock(const std::shared_ptr<Block> &node) = 0;
-    virtual void VisitDeclaration(const std::shared_ptr<Declaration> &node) = 0;
-    virtual void VisitVarDecl(const std::shared_ptr<VarDeclaration> &node) = 0;
-    virtual void VisitCompound(const std::shared_ptr<Compound> &node) = 0;
-    virtual void VisitBinOp(const std::shared_ptr<BinOp> &node) = 0;
-    virtual void VisitOper(const std::shared_ptr<Oper> &node) = 0;
-    virtual void VisitNum(const std::shared_ptr<Num> &node) = 0;
-    virtual void VisitType(const std::shared_ptr<Type> &node) = 0;
-    virtual void VisitAssign(const std::shared_ptr<Assign> &node) = 0;
-    virtual void VisitVar(const std::shared_ptr<Var> &node) = 0;
-    virtual void VisitNoOp(const std::shared_ptr<NoOp> &node) = 0;
+    virtual void VisitProgram(const shared_ptr<Program> &node) = 0;
+    virtual void VisitSubprogram(const shared_ptr<Subprogram> &node) = 0;
+    virtual void VisitFunction(const shared_ptr<Function> &node) = 0;
+    virtual void VisitBlock(const shared_ptr<Block> &node) = 0;
+    virtual void VisitDeclaration(const shared_ptr<Declaration> &node) = 0;
+    virtual void VisitConst(const shared_ptr<Var> &node) = 0;
+    virtual void
+    VisitConstDeclaration(const shared_ptr<ConstDeclaration> &node) = 0;
+    virtual void
+    VisitArrayDeclaration(const shared_ptr<ArrayDeclaration> &node) = 0;
+    virtual void VisitVarDecl(const shared_ptr<VarDeclaration> &node) = 0;
+    virtual void VisitCompound(const shared_ptr<Compound> &node) = 0;
+    virtual void VisitBinOp(const shared_ptr<BinOp> &node) = 0;
+    virtual void VisitOper(const shared_ptr<Oper> &node) = 0;
+    virtual void VisitNum(const shared_ptr<Num> &node) = 0;
+    virtual void VisitType(const shared_ptr<Type> &node) = 0;
+    virtual void VisitAssign(const shared_ptr<Assign> &node) = 0;
+    virtual void VisitVar(const shared_ptr<Var> &node) = 0;
+    virtual void VisitNoOp(const shared_ptr<NoOp> &node) = 0;
 };
 } // namespace code_generation
 } // namespace pascal2c
