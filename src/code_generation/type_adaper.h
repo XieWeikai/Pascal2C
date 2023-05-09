5c0f7d28502194ff75407d42cb2c5cfd24f107ef
Micuks <sdkjwql@gmail.com>
2023-05-09 20:04:54 +0800

    [generator] feat: Add convert from string to VarType method

--- src/code_generation/type_adaper.h
+++ src/code_generation/type_adaper.h
@@ -19,7 +19,12 @@ enum VarType {
 class TypeToolKit {
   public:
     const string SymbolToC(const string &pascal_symbol) const;
-    const string VarTypeToString(VarType var_type) const;
+    const string VarTypeToString(VarType var_type) const {
+        return VarTypeToStringMap.at(var_type);
+    }
+    VarType StringToVarType(const string &s) const {
+        return StringToVarTypeMap.at(s);
+    }
 
   private:
     const hashmap<string, string> PascalToCTypeMap = {
@@ -30,6 +35,15 @@ class TypeToolKit {
         {VarType::VOID, "void"},     {VarType::CHAR, "char"},
         {VarType::STRING, "string"}, {VarType::INT, "int"},
         {VarType::REAL, "real"},     {VarType::UNDEFINED, "undefined"}};
+    const hashmap<string, VarType> StringToVarTypeMap = {
+        {"void", VarType::VOID},
+        {"char", VarType::CHAR},
+        {"string", VarType::STRING},
+        {"int", VarType::INT},
+        {"real", VarType::REAL},
+        {"double", VarType::REAL},
+        {"undefined", VarType ::UNDEFINED},
+    };
 };
 } // namespace code_generation
 } // namespace pascal2c
